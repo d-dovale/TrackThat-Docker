65fe4739c1cc6ae9812e011ece9bc1a6581e8b8a
mauricio.b.mossi <mauricio.b.mossi@gmail.com>
2024-11-08 10:55:55 -0500

    fixed broken import

--- TrackThat-Front-End/src/Components/navbar.jsx
+++ TrackThat-Front-End/src/Components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import images from "../images";
-import './Navbar.css';
+import './navbar.css';
 
 function Navbar() {
   const navigate = useNavigate();
