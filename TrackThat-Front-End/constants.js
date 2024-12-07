4460999fe6e3ff78722a9491479d04c60ee21ab7
mauricio.b.mossi <mauricio.b.mossi@gmail.com>
2024-12-06 23:38:36 -0500

    success url test

--- TrackThat-Front-End/constants.js
+++ TrackThat-Front-End/constants.js
@@ -1,10 +1,7 @@
-const base = import.meta.env.VITE_URL || "127.0.0.1";
+const base = import.meta.env.VITE_URL || "http://127.0.0.1:8000";
 
-console.log("Constants: ===========", base)
-console.log("Constants: ===========", import.meta.env.VITE_URL)
-
-export const SIGNUPURL = `http://${base}:8000/auth/signup`
-export const LOGINURL = `http://${base}:8000/auth/login-email`
-export const SETGOALURL = `http://${base}:8000/auth/set-goal`
-export const PATCHUSERURL = `http://${base}:8000/auth/patch-user`
-export const APPLICATIONSURL = `http://${base}:8000/applications`
+export const SIGNUPURL = `${base}/auth/signup`
+export const LOGINURL = `${base}/auth/login-email`
+export const SETGOALURL = `${base}/auth/set-goal`
+export const PATCHUSERURL = `${base}/auth/patch-user`
+export const APPLICATIONSURL = `${base}/applications`
