fe10c67b4150b5b79e392418e241741782e340ef
mauricio.b.mossi <mauricio.b.mossi@gmail.com>
2024-12-06 23:26:49 -0500

    added vars, modified prod docker-compose

--- TrackThat-Front-End/constants.js
+++ TrackThat-Front-End/constants.js
@@ -1,5 +1,9 @@
-export const SIGNUPURL = "http://127.0.0.1:8000/auth/signup"
-export const LOGINURL = "http://127.0.0.1:8000/auth/login-email"
-export const SETGOALURL = "http://127.0.0.1:8000/auth/set-goal"
-export const PATCHUSERURL = "http://127.0.0.1:8000/auth/patch-user"
-export const APPLICATIONSURL = "http://127.0.0.1:8000/applications"
+const base = import.meta.env.VITE_URL || "127.0.0.1";
+
+console.log("Constants: ===========", base)
+
+export const SIGNUPURL = `http://${base}:8000/auth/signup`
+export const LOGINURL = `http://${base}:8000/auth/login-email`
+export const SETGOALURL = `http://${base}:8000/auth/set-goal`
+export const PATCHUSERURL = `http://${base}:8000/auth/patch-user`
+export const APPLICATIONSURL = `http://${base}:8000/applications`
